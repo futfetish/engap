25e5603b0d7b49af95db21e2b9090f0f19c3d3be
futfetish <gr1zzly.medvegonok@gmail.com>
2024-04-27 14:31:44 +0300

    word types

--- src/types/word.ts
+++ src/types/word.ts
@@ -2,3 +2,19 @@ import { z } from "zod";
 
 export const Zlang = z.enum(['eng' , 'rus'])
 
+export type Lang = z.infer<typeof Zlang>
+
+export interface Word  {
+    id: number;
+    word: string;
+    createdAt: Date;
+    language: Lang;
+    remembered: boolean;
+}
+
+export interface WordDTO {
+    id: number;
+    word: string;
+    language: Lang;
+    remembered: boolean;
+}
