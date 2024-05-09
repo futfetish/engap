ccfd9f9e351d17133d4becec89ad1311e9e38f95
futfetish <gr1zzly.medvegonok@gmail.com>
2024-05-09 18:13:54 +0300

    tw colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,20 @@ export default {
       fontFamily: {
         sans: ["var(--font-sans)", ...fontFamily.sans],
       },
+      colors : {
+        primary : {
+          1 : '#F5F5F5',
+          2 : "#fff9",
+          3 : "#ffffff24",
+          4 : "#282828",
+          5 : "#1A1A1A",
+        },
+        secondary : {
+          1 : "#ffa1161f",
+          2 : "#ffa116",
+        },
+        green : "#2cbb5e",
+      }
     },
   },
   plugins: [],
