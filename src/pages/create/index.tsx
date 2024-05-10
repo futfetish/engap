99945efb8853fcc831212d7b3537175ab917b706
futfetish <gr1zzly.medvegonok@gmail.com>
2024-05-10 13:01:04 +0300

    create page

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,35 +1,68 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { Lang } from "~/types/word";
+import { Title } from "~/components/title";
+import { Layout } from "~/features/layout";
+import { Lang, Word } from "~/types/word";
 import { api } from "~/utils/api";
 import { cn } from "~/utils/cn";
 
 export default function CreateP() {
   const router = useRouter();
-  const [value, setValue] = useState("");
-  const [lang, setLang] = useState<Lang>("eng");
+  const [word, setWord] = useState("");
+  const [language, setLanguage] = useState<Lang>("eng");
   const { mutate: createF } = api.word.create.useMutation({
-    onSuccess: (data) => {
+    onSuccess: (data: Word) => {
       router.push("/words/" + data.id);
     },
   });
+  const createWord = () => {
+    createF({word , language})
+  }
   return (
-    <div className="flex flex-col gap-4">
-      <div className="flex gap-2">
-        <button onClick={() => setLang('eng')} className={cn( 'px-4' , 'rounded-md' ,lang == 'eng' ? 'bg-slate-200' : '')}>eng</button>
-        <button onClick={() => setLang('rus')} className={cn( 'px-4' , 'rounded-md' ,lang == 'rus' ? 'bg-slate-200' : '')} >rus</button>
+    <Layout>
+     
+      <div className="flex flex-col gap-4">
+         <Title>create</Title>
+        <div className="flex gap-2 text-primary-2">
+          <button
+            onClick={() => setLanguage("eng")}
+            className={cn(
+              "px-4 rounded-md",
+              language == "eng" ? "bg-primary-4 text-primary-1" : "",
+            )}
+          >
+            eng
+          </button>
+          <button
+            onClick={() => setLanguage("rus")}
+            className={cn(
+              "px-4 rounded-md",
+              language == "rus" ? "bg-primary-4 text-primary-1" : "",
+            )}
+          >
+            rus
+          </button>
+        </div>
+        <div className="flex gap-2">
+          <div className="flex gap-2">
+            <input
+              type="text"
+              value={word}
+              onChange={(e) => setWord(e.currentTarget.value)}
+              placeholder="word"
+              className="w-full rounded-[4px] bg-primary-3 p-[6px] focus-visible:outline-0"
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  createWord();
+                }
+              }}
+            />
+          </div>
+          <button onClick={() =>createWord()}>
+            create
+          </button>
+        </div>
       </div>
-      <div className="flex gap-2">
-        <input
-          value={value}
-          onChange={(e) => setValue(e.currentTarget.value)}
-          type="text"
-          className="rounded-[2px] border-[1px] border-black px-2"
-        />
-        <button onClick={() => createF({ word: value, language: lang })}>
-          create
-        </button>
-      </div>
-    </div>
+    </Layout>
   );
 }
