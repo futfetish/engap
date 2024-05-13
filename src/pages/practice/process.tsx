851aee315e9856a74c3469c1b467f5c33ed562a5
futfetish <gr1zzly.medvegonok@gmail.com>
2024-05-13 21:51:40 +0300

    practice process settings

--- src/pages/practice/process.tsx
+++ src/pages/practice/process.tsx
@@ -1,35 +1,92 @@
+import { Dices, Shuffle } from "lucide-react";
 import { useRouter } from "next/router";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Title } from "~/components/common/title";
 import { PickButton } from "~/components/ui/pickButton";
 import { Layout } from "~/features/layout/layout";
-import { Lang, PracticeLang } from "~/types/word";
+import { Mode, ZMode, ZPracticeLang } from "~/types/practice";
+import { PracticeLang } from "~/types/practice";
 
 export default function PracticeProcessP() {
-  const router = useRouter();
-  console.log(router.query);
+  //   const router = useRouter();
+  //   console.log(router.query);
   return (
     <Layout page="Practice">
       <div className="flex flex-col gap-4">
-        <div>
+        <div className="flex flex-col gap-2">
           <Title>practice</Title>
+          <PracticeSettings />
         </div>
       </div>
     </Layout>
   );
 }
 
+const PracticeSettings: FC = () => {
+  const router = useRouter();
+  router.query;
+  const [language, setLanguage] = useState<PracticeLang | undefined>(undefined);
+  const [mode, setMode] = useState<Mode | undefined>(undefined);
 
-const PracticeSettings : FC = () => {
-    const [language, setLanguage] = useState<PracticeLang>("both");
-    return <div>
-          <div className="flex gap-2 text-primary-2">
-          <PickButton className="w-[100px]" value={'english'} pick={language} setPick={setLanguage}>
-            eng
-          </PickButton>
-          <PickButton className="w-[100px]" value={'russian'} pick={language} setPick={setLanguage}>
-            rus
-          </PickButton>
-        </div>
-    </div>
-}
+  useEffect(() => {
+    if (language == undefined && mode == undefined) {
+      try {
+        const MODE = ZMode.parse(router.query.mode);
+        const LANGUAGE = ZPracticeLang.parse(router.query.language);
+        setLanguage(LANGUAGE);
+        setMode(MODE);
+      } catch {
+        router.push("/practice");
+      }
+    }
+
+    if (router.query.mode && router.query.language && mode && language) {
+      if (
+        (language !== router.query.language || mode !== router.query.mode)
+      ) {
+        router.push(`/practice/process?language=${language}&mode=${mode}`);
+      }
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [language, mode, router.query.language , router.query.mode]);
+
+  return (
+    <>
+      <div className="flex gap-2 text-primary-2">
+        <PickButton
+          className="px-4"
+          value={"english"}
+          pick={language}
+          setPick={setLanguage}
+        >
+          eng
+        </PickButton>
+        <PickButton
+          className="px-4"
+          value={"russian"}
+          pick={language}
+          setPick={setLanguage}
+        >
+          rus
+        </PickButton>
+        <PickButton
+          className="px-4"
+          value={"both"}
+          pick={language}
+          setPick={setLanguage}
+        >
+          both
+        </PickButton>
+      </div>
+
+      <div className="flex gap-2 text-primary-2">
+        <PickButton value={"shuffle"} pick={mode} setPick={setMode}>
+          <Shuffle />
+        </PickButton>
+        <PickButton value={"random"} pick={mode} setPick={setMode}>
+          <Dices />
+        </PickButton>
+      </div>
+    </>
+  );
+};
